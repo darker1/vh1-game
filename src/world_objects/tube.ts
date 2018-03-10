d2107089df4dcbf55f7b4951efa64b0b7478c89b
Kyle Wall <kyle.wall@hyperloop-one.com>
2018-03-09 17:52:30 -0800

    figuring out tube

--- src/world_objects/tube.ts
+++ src/world_objects/tube.ts
@@ -23,18 +23,9 @@ export class Tube extends Sprite {
   }
 
   protected drawSprite(which: string, sprite: SpriteLocation, destination: SpriteLocation, ctx: CanvasRenderingContext2D) {
-    /*const distance = Math.sqrt(Math.pow(this._startLocation.x - this._endLocation.x, 2) + Math.pow(this._startLocation.y - this._endLocation.y, 2));
-    const loc: SpriteLocation = { x: this._startLocation.x, y: this._startLocation.y, w: this._startLocation.w, h: this._startLocation.h };
-
-    //const slope = (this._startLocation.y - this._endLocation.y) / (this._startLocation.x - this._endLocation.x);
-
-    for (let i = 0; i < distance; i++) {
-      //step along the line via the slope
-      loc.x = loc.x + 1;
-     // loc.y = loc.y + slope;
-
-      super.drawSprite(which, sprite, loc, ctx);
-  }*/
+    for (let i = 0; i < this._walls.length; i++) {
+      this._walls[i].
+    }
   }
 
 }
---
+++ src/world_objects/world.ts
@@ -0,0 +1,9 @@
+export class World { 
+  public constructor(public width: number, public height: number) {
+  }
+
+  public generateHills(n: number) {
+    
+  }
+
+}
